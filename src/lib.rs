77b56de0550b1c425190adab856ed69a340e5827
Bob Forsman <bob.forsman@ericsson.com>
2025-03-28 12:55:58 -0400

    add mut_clown_car_ref() demo

--- src/lib.rs
+++ src/lib.rs
@@ -95,4 +95,31 @@ impl<T> ClownCar<T> {
         Self::use_mut_ref(&mut *arc_mutex.lock().unwrap());
         Self::use_mut_ref(&mut *arc_rw_lock.write().unwrap());
     }
+
+
+    pub fn mut_clown_car_ref(
+        cell: &mut Cell<T>,
+        ref_cell: &RefCell<T>,
+        my_box: &mut Box<T>,
+        ref_count: &mut Rc<T>, // extremely limited.  requires Rc::is_unique() ; that this be the singular instance (reference count == 1)
+        ref_count_cell: &mut Rc<Cell<T>>, // extremely limited.  requires Rc::is_unique() ; that this be the singular instance (reference count == 1)
+        ref_count_refcell: &Rc<RefCell<T>>,
+        mutex: &mut Mutex<T>,
+        rw_lock: &RwLock<T>,
+        arc: &mut Arc<T>, // doomed
+        arc_mutex: &Arc<Mutex<T>>,
+        arc_rw_lock: &Arc<RwLock<T>>,
+    ) {
+        Self::use_mut_ref(cell.get_mut());
+        Self::use_mut_ref(&mut *ref_cell.borrow_mut());
+        Self::use_mut_ref(my_box);
+        Self::use_mut_ref(Rc::get_mut(ref_count).unwrap());
+        Self::use_mut_ref(Rc::get_mut(ref_count_cell).unwrap().get_mut());
+        Self::use_mut_ref(&mut *(*ref_count_refcell).borrow_mut());
+        Self::use_mut_ref(&mut *mutex.get_mut().unwrap());
+        Self::use_mut_ref(&mut *rw_lock.write().unwrap());
+        // not possible to get &mut T from Arc<T> Self::use_mut_ref(&mut *arc);
+        Self::use_mut_ref(&mut *arc_mutex.lock().unwrap());
+        Self::use_mut_ref(&mut *arc_rw_lock.write().unwrap());
+    }
 }
